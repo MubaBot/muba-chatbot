5237c257257f5a12ec828ef1e6b0f90c3a4499b4
say2 <say2@github.com>
2018-11-13 17:13:40 +0900

    fix algorithm and adding manager function

--- app.py
+++ app.py
@@ -1,15 +1,42 @@
-from flask import Flask
+from flask import Flask,redirect
 from flask import request,make_response,render_template
 import json
 from chatbot_api.chat_response import *
+import sqlite3
+from flask import _app_ctx_stack
+import os
+
 
 app = Flask(__name__)
+DATABASE = os.getcwd()+'/chatbotdb.db'
 
+def get_db():
+    top = _app_ctx_stack.top
+    if not hasattr(top, 'sqlite_db'):
+        top.sqlite_db = sqlite3.connect(DATABASE)
+    return top.sqlite_db
+
+def query_execute(q,arg):
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute(q,arg)
+    conn.commit()
+    cur.close()
+
+@app.teardown_appcontext
+def close_connection(exception):
+    top = _app_ctx_stack.top
+    if hasattr(top, 'sqlite_db'):
+        top.sqlite_db.close()
+
+@app.route('/chatbot/chatbot')
+def chatbot():
+    response = render_template('dashboard.html', foo=42)
+    return response
 
 @app.route('/')
 def index():
-    response = render_template('dashboard.html', foo=42)
-    return response
+    return redirect('/chatbot/db_manage')
 
 
 @app.route('/chatbot/api/get_message')
@@ -20,14 +47,7 @@ def get_message():
 
     argv=request.args.get('argv')
     argv = json.loads(argv)
-    print(msg_txt,scenario,intent_history,argv)
-    # print(msg_txt,data_dic,intent)
-    # if(argv):
-    #     argv=json.loads(argv)
-    # else:
-    #     argv={}
-    # res_txt = predict(np.array(inference_embed(msg_txt)).flatten())
-    # print(muba_response(msg_txt, scenario, intent_history, argv))
+    print('hi'+msg_txt,scenario,intent_history,argv)
     muba_msg, func, cur_scenario, intent_history,argv=muba_response(msg_txt,int(scenario),intent_history,argv)
     argv=json.dumps(argv)
     #print("@"*100)
@@ -36,6 +56,102 @@ def get_message():
     print(ret)
     return ret
 
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.route('/chatbot/db_manage')
+def db_manage():
+    tables = ['scenario', 'muba_response_def', 'user_request_def', 'response_func']
+    cur_table = request.args.get('table', default='muba_response_def', type=str)
+    intent_list=[]
+    if cur_table=='muba_response_def':
+        response_def=query_db('select * from muba_response_def')
+        for idx,rd in enumerate(response_def):
+
+            x=query_db('select intent_name,func from muba_response_intent where muba_response_intent_id  = ?',[rd[1]],one=True)
+            # print(x)
+            if len(x)==2:
+                response_def[idx]=rd+(x[0],x[1],)
+        table_info=response_def
+        intent_list=query_db('select muba_response_intent_id,intent_name from muba_response_intent')
+    if cur_table=='user_request_def':
+        request_def=query_db('select * from user_request_def')
+        for idx,rd in enumerate(request_def):
+
+            x=query_db('select intent_name from user_request_intent where user_request_intent_id  = ?',[rd[1]],one=True)
+            # print(x)
+            if len(x)==1:
+                request_def[idx]=rd+(x[0],)
+        table_info=request_def
+        intent_list = query_db('select user_request_intent_id,intent_name from user_request_intent')
+    if cur_table=='scenario':
+        user_intent = query_db('select user_request_intent_id,intent_name from user_request_intent')
+        muba_intent = query_db('select muba_response_intent_id,intent_name from muba_response_intent')
+        scenario=query_db('select * from scenario')
+        idx=0
+        for id,scene in scenario:
+            # print(scene)
+            scene=scene.split(',')[1:]
+            intent_str_list=[]
+
+
+            for i,s in enumerate(scene):
+                if i%2==1:
+                    x=query_db('select intent_name from muba_response_intent where muba_response_intent_id=?',[s],one=True)
+                    if x:
+                        y=query_db('select sentence from muba_response_def where muba_response_intent_id=?',[s])
+
+                        intent_str_list.append(('muba',x[0],y))
+                else:
+                    x = query_db('select intent_name from user_request_intent where user_request_intent_id=?', [s],
+                                 one=True)
+                    if x:
+
+                        y = query_db('select sentence from user_request_def where user_request_intent_id=?', [s])
+
+                        intent_str_list.append(('user', x[0],y))
+            scenario[idx]=scenario[idx]+(intent_str_list,)
+            idx+=1
+        print(scenario)
+        table_info=scenario
+        response = render_template('manage.html', table_info=table_info, tables=tables, cur_table=cur_table,user_intent=user_intent,muba_intent=muba_intent)
+        return response
+
+
+
+
+    response = render_template('manage.html',table_info=table_info,tables=tables,cur_table=cur_table,intent_list=intent_list)
+    return response
+
+
+
+@app.route('/chatbot/db_manage/item_add',methods=['POST'])
+def item_add():
+    table_name=request.form['cur_table']
+    id=request.form.get('intent_id',type=int)
+    sentence=request.form.get('sentence',type=str)
+    if id and sentence:
+        if table_name=='muba_response_def':
+            query_execute('insert into muba_response_def (muba_response_intent_id,sentence) values (?, ?);',[id,sentence])
+        if table_name=='user_request_def':
+            query_execute('insert into user_request_def (user_request_intent_id,sentence) values (?, ?);',[id,sentence])
+
+    return redirect('/chatbot/db_manage?table='+table_name)
+
+@app.route('/chatbot/db_manage/item_delete')
+def item_delete():
+    table_name=request.args.get('table')
+    id=request.args.get('id')
+    if table_name=='muba_response_def' and id:
+        print('hihi')
+        query_execute('delete from muba_response_def where sentence_id=(?)',[id])
+    elif table_name=='user_request_def' and id:
+        query_execute('delete from user_request_def where sentence_id=?', [id])
+
+    return redirect('/chatbot/db_manage?table='+table_name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- chatbot_api/chat_response.py
+++ chatbot_api/chat_response.py
@@ -11,7 +11,10 @@ MR = Markov_response()
 def muba_response(msg, cur_scenario, priv_intent,argv):
     # 메시지가 음식 이름일때 -> 그 음식을 판매하는 근처의 음식점 보여주기
     orig_msg=msg
-    msg,restaurant=replace_restaurant(msg)
+    if 'restaurant' in argv.keys():
+        restaurant=argv['restaurant']
+    else:
+        msg,restaurant=replace_restaurant(msg)
 
     if restaurant:
         if 'restaurant' in argv.keys():
@@ -30,18 +33,23 @@ def muba_response(msg, cur_scenario, priv_intent,argv):
             argv['food'] = food + argv['food']
         else:
             argv['food'] = food
-    print(msg,argv)
+    # print(msg,argv)
+    # print('scenario'+str(cur_scenario))
+    # print('priv_inent',priv_intent)
     intent=find_intent_hard(msg)
     if intent:
 
         if(intent_scenario_conformity_check(cur_scenario,priv_intent+[intent])):
-            print(cur_scenario,len(priv_intent),argv)
+            # print('ho',cur_scenario,len(priv_intent),argv)
             priv_intent.append(intent)
             muba_msg,func,muba_intent=make_response_base_scenario(cur_scenario,len(priv_intent),argv)
             priv_intent.append(muba_intent)
             return muba_msg,func,cur_scenario,priv_intent,argv
         else:
             scenario_candidate_list=find_scenario_base_priv_intent(priv_intent+[intent])
+            # print(scenario_candidate_list)
+            # print(priv_intent+[intent])
+
             if len(scenario_candidate_list)>0:
                 cur_scenario=scenario_candidate_list[0]
                 priv_intent.append(intent)
@@ -50,7 +58,9 @@ def muba_response(msg, cur_scenario, priv_intent,argv):
                 return muba_msg, func, cur_scenario, priv_intent,argv
             else:
                 #시나리오에 없는.. 시나리오 -> 새로운 시나리오 시작
+                ##TODO bug 처음부터 없는거 들어오면 다 무한루프)
                 new_argv={'user':argv['user']}
+                # input('>')
                 return muba_response(orig_msg,-1,['1',],new_argv)#muba_msg, func, cur_scenario, priv_intent
 
     else:
